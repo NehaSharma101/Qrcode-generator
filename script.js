62940b09f49e36ee46b823b4b991b2cbe0165a90
Neha Sharma <85702915+NehaSharma101@users.noreply.github.com>
2023-06-11 02:13:14 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var container=document.querySelector(".container");
+var generateBtn=document.querySelector(".generate_btn");
+var qrInput=document.querySelector(".qr_input");
+var qrImg=document.querySelector(".qr_image");
+generateBtn.onclick=function(){
+    container.classList.add("active");
+}
